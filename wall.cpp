46137fb1f4ad97736352906954d41a9723e26ad2
darkstrom <john.kalmykov@gmail.com>
2024-01-10 10:47:33 +0100

    merge

--- wall.cpp
+++ wall.cpp
@@ -2,10 +2,13 @@
 
 std::vector<wall*> wall::objList;
 
-wall::wall(sf::Vector2f begPos1, sf::Vector2f endPos1){
+wall::wall(sf::Vector2f begPos, sf::Vector2f endPos) {
+
     objList.push_back(this);
-    begPos = begPos1;
-    endPos = endPos1;
+    m = (endPos.y - begPos.y) / (endPos.x - begPos.x);
+    b = begPos.y - m * begPos.x;
+    this->begPos = begPos;
+    this->endPos = endPos;
 }
 
 void wall::draw(sf::RenderTarget& target, sf::RenderStates states) const {
