a86cb27e39343e5f9a007c4091face62553db0d0
darkstrom <john.kalmykov@gmail.com>
2024-01-10 10:46:13 +0100

    merge test and master

--- rays.h
+++ rays.h
@@ -10,17 +10,12 @@ public:
 	sf::Color color;
 
 	rays(sf::Vector2f pos, int numOfRays, sf::Color colour);
-		
+
 private:
-	sf::Vector2f calc_hit(sf::Vector2f wallBegPos, sf::Vector2f wallEndPos, sf::Vector2f lineBegPos, sf::Vector2f lineEndPos);
+	sf::Vector2f calc_hit(sf::Vector2f& lineBegPos, sf::Vector2f& lineEndPos, wall& wall);
 	float get_distance(sf::Vector2f pos1, sf::Vector2f pos2);
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
 	sf::Vector2f closest_hit(std::vector<wall*> walls, sf::Vector2f& lineBegPos, sf::Vector2f& lineEndPos);
 	bool is_on_segment(sf::Vector2f& beg, sf::Vector2f& end, sf::Vector2f& point);
 
-	const float tolerance = 0.01f;
 };
-	
-
-
-
